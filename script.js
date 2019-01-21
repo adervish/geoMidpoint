4293287b92f3a1d6746580791cb7979fa69e2827
Alexander Derbes <acd@alexandsmacbook.lan>
2019-01-21 17:20:50 -0500

    added center min distnace

--- script.js
+++ script.js
@@ -1,34 +1,96 @@
 
-/*function geoLocate() {
-  //var spreadsheetId = '1743HV5Mt01NaB5r_QjgsVjjsFEcXQIYFTeqUVzuGFBE';
-  var rangeName = 'Form Responses 1!A2:J30';
+var EARTH_RADIUS = 3958.756;
+
+/**
+ * Generate eight test points around a center point N,NE,E,SE,S,SW,E,NW a specified distance from the center point
+ *
+ * @param {number} point in Radians           Description.
+ * @param {type}   [var]         Description of optional variable.
+ * @param {type}   [var=default] Description of optional variable with default variable.
+ * @param {Object} objectVar     Description.
+ * @param {type}   objectVar.key Description of a key in the objectVar parameter.
+ * 
+ * @return {type} Description.
+ */
+function gen8TestPoints(lat, lng, dist)
+{
+  φ1 = toRadians(lat);
+  λ1 = toRadians(lng);
+  
+  var result = [];
+  var brng = Math.PI;
+  for( var i =0; i<8; i++)
+  {
+    var brng = 2*Math.PI/8 * i;
+    var φ2 = Math.asin( Math.sin( φ1 )*Math.cos(dist) +
+                    Math.cos(φ1)*Math.sin(dist)*Math.cos(brng) );
+    var λ2 = λ1 + Math.atan2(Math.sin(brng)*Math.sin(dist)*Math.cos(φ1),
+                         Math.cos(dist)-Math.sin(φ1)*Math.sin(φ2));
+    ary = [toDegrees(φ2), toDegrees(λ2)];
+    result[i] = ary;
+  }
+  return result;
+}
+
+function test()
+{
+  var rangeName = 'Sheet1!B2:C32';
   var range = SpreadsheetApp.getActiveSpreadsheet().getRange(rangeName);
   var values = range.getValues();
 
-  //var values = Sheets.Spreadsheets.Values.get(spreadsheetId, rangeName).values;
-  if (!values) {
-    Logger.log('No data found.');
-  } else {
-    Logger.log('Name, Major:');
-    for (var row = 0; row < values.length; row++) {
-      // Print columns A and E, which correspond to indices 0 and 4.
-      Logger.log(' - %s, %s', values[row][2], values[row][3]);
-      
-      var response = Maps.newGeocoder()
-    // The latitudes and longitudes of southwest and northeast corners of Colorado, respectively.
-    .setBounds(40.66661, -74.16792, 40.78526, -73.78202)
-    .geocode(values[row][2] + ' and ' + values[row][3] + ' ' + values[row][3] + ' ' + values[row][4] );
-      
-      
-      values[row][8] = response.results[0].geometry.location.lat;
-      values[row][9] = response.results[0].geometry.location.lng;
-      range.setValues(values);
+  Logger.log("in test()");
+  centerMinimumDistance(values);
+}
 
-     Logger.log(response.results[0].geometry.location );
+function centerMinDistance(values)
+{
+  var midPoint = geoMidpoint(values);
+  var curPoint = midPoint;
+  totalDistance = sumDistance(midPoint[0], midPoint[1], values);
 
+  // check to see if one of the existing points it closer 
+  for each (var r in values)
+  {
+    var ptDistance = sumDistance(r[0], r[1], values);
+    if( ptDistance < totalDistance)
+    {
+      curPoint = r;
+      totalDistance = ptDistance;
+      Logger.log("test() -- NEW CLOSEST POINT " + r);
     }
   }
-}*/
+  
+  var testDistance = Math.PI/2000;
+  
+  while( testDistance > 0.00000002 )
+  {
+    var newPoint = 0;
+    var testPoints = gen8TestPoints(curPoint[0], curPoint[1], testDistance );
+    for each (var t in testPoints )
+    {
+      var ptDistance = sumDistance(t[0], t[1], values);
+      //Logger.log("ptDistance = " + ptDistance );
+      if( ptDistance < totalDistance )
+      {
+        totalDistance = ptDistance;
+        curPoint = t;
+        newPoint = 1;
+        Logger.log("new point = " + curPoint);
+      }
+    }
+    if( newPoint == 0 )
+    {
+      testDistance = testDistance / 2;
+      Logger.log("new test distance = " + testDistance);
+    }
+  }
+  
+  Logger.log("final point = " + curPoint);
+  return curPoint;
+  
+
+    
+}
 
 // When user installs spreadsheet, add Spreadsheet Mapper menu. 
 function onInstall() { 
@@ -48,9 +110,25 @@ function addSpreadsheetGeoMenu() {
   ss.addMenu("Geo", menuEntries);
 }
 
-function distance(lat1, lon1, lat2, lon2)
+function toRadians (angle) {
+  return angle * (Math.PI / 180);
+}
+
+function toDegrees (angle) {
+  return angle * (180 / Math.PI);
+}
+
+
+function sphereDistanceR(lat1, lon1, lat2, lon2)
 {
-  return Math.acos(Math.sin(lat1)*Math.sin(lat2) + Math.cos(lat1)*Math.cos(lat2)*Math.cos(lon2 - lon1));
+  var v =  Math.acos(Math.sin(lat1)*Math.sin(lat2) + Math.cos(lat1)*Math.cos(lat2)*Math.cos(lon2 - lon1));
+  //Logger.log("sphereDistance() = " + v);
+  return v;
+}
+
+function sphereDistanceD(lat1, lon1, lat2, lon2)
+{
+  return sphereDistanceR( toRadians(lat1), toRadians(lon1), toRadians(lat2), toRadians(lon2));
 }
 
 function geoMidpoint(input) 
@@ -70,15 +148,15 @@ function geoMidpoint(input)
     var lng = r[1] * Math.PI/180;
     if ( ! isNaN(lat) && ! isNaN(lng) )
     {
-      t_lat += r[0];
-      t_lng += r[1];
+      t_lat = t_lat + r[0];
+      t_lng = t_lng + r[1];
       x = Math.cos(lat) * Math.cos(lng);
       y = Math.cos(lat) * Math.sin(lng);
       z = Math.sin(lat);
     
-      t_x += x;
-      t_y += y;
-      t_z += z;
+      t_x = t_x + x;
+      t_y = t_y + y;
+      t_z = t_z + z;
       len++;
     }
   } 
@@ -103,18 +181,37 @@ function geoMidpoint(input)
   return [lat_r, lng_r];
 }
 
+function distanceBetweenPoints(p1x, p1y, p2x, p2y)
+{
+   
+  var val = Math.sqrt( (p1x - p2x)*(p1x - p2x) + (p1y - p2y)*(p1y - p2y));
+  return val;
+}
+
+function sumDistance(px, py, input)
+{
+  var totalDistance = 0;
+  for each (var r in input)
+  {
+    totalDistance = totalDistance + sphereDistanceD(px, py, r[0], r[1]);
+  }
+  //Logger.log( "sumDistance() = " + totalDistance );
+  return totalDistance;
+}
+
 function geoMinDistance(input)
 {
   var start = geoMidpoint(input);
   var s_lat = start[0];
   var s_lng = start[1];
   
+  return sumDistance(s_lat, s_lng, input);
+  
   var testDistance = Math.PI / 2;
   while (testDistance > 0.00000002 )
   {
     testDistance = testDistance / 2;
   }
-  
 }
 
 
